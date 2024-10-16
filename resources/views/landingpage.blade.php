4eda7627186042a7dba51f1ebbd83f7eb725e44c
matheusj12 <matheusjulio12@hotmail.com>
2024-10-15 23:56:41 -0300

    15102024

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -8,24 +8,23 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/landingpage.css') }}">
-
-
 </head>
 
-
+<body>
     <header class="bg-light fixed-top py-3 shadow">
         <div class="container">
             <div class="row align-items-center">
-                <!-- Primeira coluna: Logo e nome da empresa no canto esquerdo -->
+                <!-- Logo e nome da empresa -->
                 <div class="col-6 col-md-3 text-start">
                     <img src="{{ asset('src/Frame.png') }}" alt="Logo" class="img-fluid ms-1">
                     <span class="h4 ms-3">Lalasia</span>
                 </div>
 
-                <!-- Segunda coluna: Navegação centralizada -->
+                <!-- Navegação -->
                 <div class="col-6 col-md-6">
                     <nav class="navbar navbar-expand-lg navbar-light justify-content-center">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarNav">
@@ -47,7 +46,7 @@
                     </nav>
                 </div>
 
-                <!-- Terceira coluna: Ícones à direita -->
+                <!-- Ícones à direita -->
                 <div class="col-12 col-md-3 d-none d-md-flex justify-content-end">
                     <div class="d-flex">
                         <a href="#" class="btn me-1"><i class="fas fa-shopping-cart"></i></a>
@@ -57,39 +56,31 @@
             </div>
         </div>
     </header>
-<body>
-<div class="container mt-5 pt-5">
-    <div class="row justify-content-center text-center mt-2">
-        <div class="col-12">
+
+    <div class="container mt-5 pt-5">
+    <div class="row h-100">
+        <!-- Seta à esquerda -->
+        <div class="col-12 col-md-2 d-flex align-items-center justify-content-center mb-3 mb-md-0">
+            <img src="{{ asset('src/seta.png') }}" alt="Seta" class="seta img-fluid">
+        </div>
+
+        <!-- Coluna central: Texto e título -->
+        <div class="col-12 col-md-8 d-flex flex-column justify-content-center text-center">
             <h1 class="title">
-                Discover Furniture With
-                <br>
-                High Quality Wood
+                Discover Furniture With<br>High Quality Wood
                 <img src="{{ asset('src/estrela.png') }}" alt="Estrela" class="star">
             </h1>
+            <h5 class="description">Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing mauris non. Purus parturient viverra nunc, tortor sit laoreet.</h5>
         </div>
-    </div>
 
-    <div class="row mt-2">
-        <div class="col-12 text-center">
-            <img src="{{ asset('src/seta.png') }}" alt="seta" class="seta">
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-12">
-            <h5 class="description">
-                Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi,<br>
-                adipiscing mauris non. Purus parturient viverra nunc, tortor sit laoreet. Quam tincidunt <br>
-                aliquam adipiscing tempor.
-            </h5>
+        <!-- Seta à direita -->
+        <div class="col-12 col-md-2 d-flex align-items-center justify-content-center mb-3 mb-md-0">
         </div>
     </div>
 </div>
 
 
 
-</div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
